7a4cbd6fc33b80a6d204852193e97ea023d70ffe
Vishal Poonia <121330381+kevinpoonia@users.noreply.github.com>
2026-01-19 18:23:45 +0530

    Create tawk.ts

---
+++ src/lib/tawk.ts
@@ -0,0 +1,12 @@
+export function loadTawk() {
+  if (typeof window === "undefined") return;
+  if ((window as any).Tawk_API) return;
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://embed.tawk.to/696e28a2687ba6197fc027ed/1hxxxxxx";
+  script.charset = "UTF-8";
+  script.setAttribute("crossorigin", "*");
+
+  document.body.appendChild(script);
+}
