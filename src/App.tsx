2e2a31d3cc7f11b6abf6903af6548d85cfee0351
Vishal Poonia <121330381+kevinpoonia@users.noreply.github.com>
2026-01-19 18:02:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import { Switch, Route } from "wouter";
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
+import { loadChatbase } from "@/lib/chatbase";
+
 
 export default function App() {
+  useEffect(() => {
+    if (import.meta.env.PROD) {
+      loadChatbase();
+    }
+  }, []);
   return (
     <Switch>
       <Route path="/" component={Home} />
