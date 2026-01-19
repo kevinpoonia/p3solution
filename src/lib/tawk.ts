06232caa0fa227a5d00eaaebc5b9e0fd5a39d6ea
Vishal Poonia <121330381+kevinpoonia@users.noreply.github.com>
2026-01-19 18:28:02 +0530

    Update tawk.ts

--- src/lib/tawk.ts
+++ src/lib/tawk.ts
@@ -4,7 +4,7 @@ export function loadTawk() {
 
   const script = document.createElement("script");
   script.async = true;
-  script.src = "https://embed.tawk.to/696e28a2687ba6197fc027ed";
+  script.src = "https://embed.tawk.to/696e28a2687ba6197fc027ed/1jfb4tf85";
   script.charset = "UTF-8";
   script.setAttribute("crossorigin", "*");
 
